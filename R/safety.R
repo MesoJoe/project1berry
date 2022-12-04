2c7a59bdcbd3c9e58b9a2962f50adfa81028212a
Joseph Berry <jberry180@gmail.com>
2022-12-03 18:48:00 -0600

    Safety Data Set

---
+++ R/safety.R
@@ -0,0 +1,12 @@
+#' safety
+#'
+#' A data set collected
+#'
+#' @format A data frame with 15 rows and 4 variables:
+#' \describe{
+#'   \item{DRIVER}{DRIVER NUMBER, DIFFERENT DRIVERS}
+#'   \item{REAC-R}{REACT-R VARIABLE}
+#'   \ITEM{REAC-U}{REAC-u VARIABLE}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"safety"
